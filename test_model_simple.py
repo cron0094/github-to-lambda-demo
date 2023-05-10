bc0d842b7309c4b1c537cbd136920476c0d858b4
Rong <chrrong@deloitte.com.au>
2023-05-11 09:49:01 +1000

    adding new specs

--- test_model_simple.py
+++ test_model_simple.py
@@ -1,4 +1,13 @@
+import json
+import sys
+
+import joblib
+import pandas as pd
+from fairlearn.metrics import MetricFrame, false_negative_rate
+from sklearn.metrics import balanced_accuracy_score
+
 x = -1
 
 if x < 0:
-  raise Exception("Sorry, no numbers below zero")
+ 
+ raise Exception("Sorry, no numbers below zero")
